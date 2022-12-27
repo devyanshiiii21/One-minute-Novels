2ebcf4cd4710578c618b3eea021b11007e2dd4ac
Devyanshi Srivastava <devyanshi2106@gmail.com>
2022-12-27 14:03:44 +0530

    Made a form to register.

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ def index():
     
     return render_template('index.html')
 
-@app.route('/greet', methods=["POST"])
-def greet():
-    name = request.form.get("name","world")
-    return render_template('greet.html',name=name)
+@app.route('/register', methods=["POST"])
+def register():
+    
+    #Validate Submission
+    if not request.form.get("name") or request.form.get("sport") not in ["Cricket", "Football", "Hockey"]:
+        return render_template("failure.html")
+    
+    #Confirm Registration
+    return render_template("success.html")
