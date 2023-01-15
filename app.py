2221a8a47143d6eb667b83ade06c03f09e5d5b70
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-01-16 01:36:47 +0530

    Changed the app model

--- app.py
+++ app.py
@@ -6,42 +6,47 @@ from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
-mail= Mail(app)
+# mail= Mail(app)
 
 # Requires that "Less secure app access" be on
 # https://support.google.com/accounts/answer/6010255
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
-app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
-mail = Mail(app)
+# app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+# app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+# app.config["MAIL_PORT"] = 587
+# app.config["MAIL_SERVER"] = "smtp.gmail.com"
+# app.config["MAIL_USE_TLS"] = True
+# app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+# mail = Mail(app)
 
-SPORTS = [
-    "Cricket",
-    "Football", 
-    "Hockey"
+posts = [
+    {
+        'author':'Collen Hoover',
+        'title': 'Ugly Love',
+        'summary':'lorem ipsum',
+        'date_posted': 'January 16th, 2023',
+    },
+    {
+        'author':'Collen Hoover',
+        'title': 'Verity',
+        'summary':'lorem ipsum',
+        'date_posted': 'January 16th, 2023',
+    },
+    {
+        'author':'Collen Hoover',
+        'title': 'It Ends With Us',
+        'summary':'lorem ipsum',
+        'date_posted': 'January 16th, 2023',
+    }
 ]
 
 @app.route("/")
+@app.route("/home")
 def index():
-    return render_template("index.html", sports=SPORTS)
+    return render_template("index.html", posts=posts )
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title= 'ABOUT')
 
-@app.route("/register", methods=["POST"])
-def register():
-
-    # Validate submission
-    name = request.form.get("name")
-    email = request.form.get("email")
-    sport = request.form.get("sport")
-    if not name or not email or sport not in SPORTS:
-        return render_template("failure.html")
-
-    # Send email
-    message = Message("You are registered!", recipients=[email])
-    mail.send(message)
-
-    # Confirm registration
-    return render_template("success.html")
+if __name__ == '__main__':
+    app.run(debug=True)
