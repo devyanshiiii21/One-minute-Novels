99597887186c596712cea7d06f4676ebc55ea1a6
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-03-20 22:28:58 +0530

    Modified the registration page

--- app.py
+++ app.py
@@ -2,14 +2,45 @@
 import os
 import re
 
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, url_for, redirect,flash
 from flask_mail import Mail, Message
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '4aaba84de24c320143526dd4df3de0dd'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=True,default='default.jpg')
+    password = db.Column(db.String(60),nullable=True)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+
+def __repr(self):
+    return f"User('{self.username}','{self.email}','{self.image.jpg}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 # mail= Mail(app)
+
+
 
 # Requires that "Less secure app access" be on
 # https://support.google.com/accounts/answer/6010255
